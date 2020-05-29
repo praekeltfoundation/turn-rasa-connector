fdd461f45d871b00872aafa633beae3253813d92
Rudi <rudi@praekelt.org>
2020-05-29 15:40:41 +0200

    Create input channel (#2)
    * Add basic setup for input channel
    
    * Add test for health endpoint
    
    * Add signature validation
    
    * Extract messages from request body
    
    * Add text message handling
    
    * Add coverage
    
    * Only upload coveralls on one test
    
    * Python 3.6 doesn't have hmac.digest
    
    * Add token for coveralls
    
    * Rather use coveralls to upload coverage
    
    * Fix indendation
    
    * Add support for media messages
    
    * Handle contact messages
    
    * Add location message type handling
    
    * Refactor

---
+++ turn_rasa_connector/test_turn.py
@@ -0,0 +1,455 @@
+from unittest import TestCase, mock
+
+from rasa.core import run, utils
+
+from turn_rasa_connector.turn import TurnInput
+
+
+class TurnInputTests(TestCase):
+    def setUp(self):
+        self.input_channel = self._create_input_channel()
+        self.app = run.configure_app([self.input_channel])
+
+    def _create_input_channel(self, hmac_secret=None):
+        return TurnInput(hmac_secret=hmac_secret)
+
+    def test_from_credentials(self):
+        """
+        Stores the credentials on the class
+        """
+        instance = TurnInput.from_credentials({"hmac_secret": "test-secret"})
+        self.assertEqual(instance.hmac_secret, "test-secret")
+
+    def test_no_credentials(self):
+        """
+        Raises an exception
+        """
+        with self.assertRaises(Exception):
+            TurnInput.from_credentials({})
+
+    def test_routes(self):
+        """
+        All routes should be set up correctly
+        """
+        routes = utils.list_routes(self.app)
+        self.assertTrue(routes.get("turn_webhook.health").startswith("/webhooks/turn"))
+        self.assertTrue(
+            routes.get("turn_webhook.webhook").startswith("/webhooks/turn/webhook")
+        )
+
+    def test_health(self):
+        """
+        Should return ok status
+        """
+        request, response = self.app.test_client.get("/webhooks/turn")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"status": "ok"})
+
+    def test_validate_signature(self):
+        """
+        Should return whether a signature is valid or not
+        """
+        self.assertFalse(
+            self.input_channel.validate_signature("secret", b"payload", "aW52YWxpZA==")
+        )
+        self.assertTrue(
+            self.input_channel.validate_signature(
+                "secret", b"payload", "uC/LeRrOxXhZuYm0MKgmSIzi5Hn9+SMmvQoug3WkK6Q="
+            )
+        )
+
+    def test_webhook_invalid_signature(self):
+        """
+        Returns a 401 with an error message
+        """
+        self.input_channel = self._create_input_channel(hmac_secret="test-secret")
+        self.app = run.configure_app([self.input_channel])
+
+        request, response = self.app.test_client.post("/webhooks/turn/webhook", json={})
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json, {"error": "invalid_signature", "success": False}
+        )
+
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook",
+            json={},
+            headers={"X-Turn-Hook-Signature": "aW52YWxpZA=="},
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json, {"error": "invalid_signature", "success": False}
+        )
+
+    def test_webhook_invalid_body(self):
+        """
+        If the body isn't a valid json object, then we should return an error message
+        """
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook", data="invalid"
+        )
+        self.assertEqual(response.status_code, 400)
+
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook", data="[]"
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"error": "invalid_body", "success": False})
+
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook", data="{}"
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"error": "invalid_body", "success": False})
+
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook", data='{"messages": "invalid"}'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"error": "invalid_body", "success": False})
+
+    def test_webhook_handle_valid_messages(self):
+        """
+        Should process the messages
+        """
+        self.app.agent = mock.Mock()
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook",
+            json={
+                "messages": [
+                    {
+                        "type": "text",
+                        "text": {"body": "message body"},
+                        "from": "27820001001",
+                        "id": "message-id",
+                        "timestamp": "1518694235",
+                    }
+                ]
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"success": True})
+
+        [call] = self.app.agent.handle_message.call_args_list
+        args, kwargs = call
+        [message] = args
+        self.assertEqual(message.text, "message body")
+        self.assertEqual(message.sender_id, "27820001001")
+        self.assertEqual(message.message_id, "message-id")
+        self.assertEqual(message.metadata, {"timestamp": "1518694235", "type": "text"})
+
+    def test_webhook_handle_invalid_messages(self):
+        """
+        Returns an invalid message error
+        """
+        request, response = self.app.test_client.post(
+            "/webhooks/turn/webhook", json={"messages": [{"type": "invalid"}]},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"success": False, "error": "invalid_message"})
+
+    def test_handle_audio(self):
+        """
+        Returns a UserMesssage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "from": "27820001001",
+                "id": "ABGGFlA5FpafAgo6tHcNmNjXmuSf",
+                "audio": {
+                    "file": "/usr/local/wamedia/shared/b1cf38-8734-4ad3-b4a1-ef0c10d0d",
+                    "id": "b1c68f38-8734-4ad3-b4a1-ef0c10d683",
+                    "mime_type": "audio/mpeg",
+                    "sha256": "29ed500fa64eb55fc19dc4124acb300e5dcc54a0f822a301ae99944",
+                },
+                "timestamp": "1521497954",
+                "type": "audio",
+            }
+        )
+        self.assertEqual(message.text, None)
+        self.assertEqual(message.sender_id, "27820001001")
+        self.assertEqual(message.message_id, "ABGGFlA5FpafAgo6tHcNmNjXmuSf")
+        self.assertEqual(
+            message.metadata,
+            {
+                "audio": {
+                    "file": "/usr/local/wamedia/shared/b1cf38-8734-4ad3-b4a1-ef0c10d0d",
+                    "id": "b1c68f38-8734-4ad3-b4a1-ef0c10d683",
+                    "mime_type": "audio/mpeg",
+                    "sha256": "29ed500fa64eb55fc19dc4124acb300e5dcc54a0f822a301ae99944",
+                },
+                "timestamp": "1521497954",
+                "type": "audio",
+            },
+        )
+
+    def test_handle_document(self):
+        """
+        Returns a UserMessage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "from": "27820001001",
+                "id": "ABGGFlA5FpafAgo6tHcNmNjXmuSf",
+                "timestamp": "1522189546",
+                "type": "document",
+                "document": {
+                    "caption": "80skaraokesonglistartist",
+                    "file": "/usr/local/wamedia/shared/fc233119-733f-49c-bcbd-b2f68f79",
+                    "id": "fc233119-733f-49c-bcbd-b2f68f798e33",
+                    "mime_type": "application/pdf",
+                    "sha256": "3b11fa6ef2bde1dd14726e09d3edaf782120919d06f6484f32d5d5c",
+                },
+            }
+        )
+        self.assertEqual(message.text, "80skaraokesonglistartist")
+        self.assertEqual(message.sender_id, "27820001001")
+        self.assertEqual(message.message_id, "ABGGFlA5FpafAgo6tHcNmNjXmuSf")
+        self.assertEqual(
+            message.metadata,
+            {
+                "document": {
+                    "file": "/usr/local/wamedia/shared/fc233119-733f-49c-bcbd-b2f68f79",
+                    "id": "fc233119-733f-49c-bcbd-b2f68f798e33",
+                    "mime_type": "application/pdf",
+                    "sha256": "3b11fa6ef2bde1dd14726e09d3edaf782120919d06f6484f32d5d5c",
+                },
+                "timestamp": "1522189546",
+                "type": "document",
+            },
+        )
+
+    def test_handle_image(self):
+        """
+        Returns a UserMessage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "from": "27820001001",
+                "id": "ABGGFlA5FpafAgo6tHcNmNjXmuSf",
+                "image": {
+                    "file": "/usr/local/wamedia/shared/b1cf38-8734-4ad3-b4a1-ef0c10d0d",
+                    "id": "b1c68f38-8734-4ad3-b4a1-ef0c10d683",
+                    "mime_type": "image/jpeg",
+                    "sha256": "29ed500fa64eb55fc19dc4124acb300e5dcc54a0f822a301ae99944",
+                    "caption": "Check out my new phone!",
+                },
+                "timestamp": "1521497954",
+                "type": "image",
+            }
+        )
+        self.assertEqual(message.text, "Check out my new phone!")
+        self.assertEqual(message.sender_id, "27820001001")
+        self.assertEqual(message.message_id, "ABGGFlA5FpafAgo6tHcNmNjXmuSf")
+        self.assertEqual(
+            message.metadata,
+            {
+                "image": {
+                    "file": "/usr/local/wamedia/shared/b1cf38-8734-4ad3-b4a1-ef0c10d0d",
+                    "id": "b1c68f38-8734-4ad3-b4a1-ef0c10d683",
+                    "mime_type": "image/jpeg",
+                    "sha256": "29ed500fa64eb55fc19dc4124acb300e5dcc54a0f822a301ae99944",
+                },
+                "timestamp": "1521497954",
+                "type": "image",
+            },
+        )
+
+    def test_handle_video(self):
+        """
+        Returns a UserMessage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "from": "27820001001",
+                "id": "ABGGFlA5FpafAgo6tHcNmNjXmuSf",
+                "video": {
+                    "file": "/usr/local/wamedia/shared/b1cf38-8734-4ad3-b4a1-ef0c10d0d",
+                    "id": "b1c68f38-8734-4ad3-b4a1-ef0c10d683",
+                    "mime_type": "video/mp4",
+                    "sha256": "29ed500fa64eb55fc19dc4124acb300e5dcc54a0f822a301ae99944",
+                    "caption": "Check out my new phone!",
+                },
+                "timestamp": "1521497954",
+                "type": "video",
+            }
+        )
+        self.assertEqual(message.text, "Check out my new phone!")
+        self.assertEqual(message.sender_id, "27820001001")
+        self.assertEqual(message.message_id, "ABGGFlA5FpafAgo6tHcNmNjXmuSf")
+        self.assertEqual(
+            message.metadata,
+            {
+                "video": {
+                    "file": "/usr/local/wamedia/shared/b1cf38-8734-4ad3-b4a1-ef0c10d0d",
+                    "id": "b1c68f38-8734-4ad3-b4a1-ef0c10d683",
+                    "mime_type": "video/mp4",
+                    "sha256": "29ed500fa64eb55fc19dc4124acb300e5dcc54a0f822a301ae99944",
+                },
+                "timestamp": "1521497954",
+                "type": "video",
+            },
+        )
+
+    def test_handle_voice(self):
+        """
+        Returns a UserMessage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "from": "27820001001",
+                "id": "ABGGFlA5FpafAgo6tHcNmNjXmuSf",
+                "timestamp": "1521827831",
+                "type": "voice",
+                "voice": {
+                    "file": "/usr/local/wamedia/shared/463e/b7ec/ff4e4d9bb1101879cbd41",
+                    "id": "463eb7ec-ff4e-4d9b-b110-1879cbd411b2",
+                    "mime_type": "audio/ogg; codecs=opus",
+                    "sha256": "fa9e1807d936b7cebe63654ea3a7912b1fa9479220258d823590521",
+                },
+            }
+        )
+        self.assertEqual(message.text, None)
+        self.assertEqual(message.sender_id, "27820001001")
+        self.assertEqual(message.message_id, "ABGGFlA5FpafAgo6tHcNmNjXmuSf")
+        self.assertEqual(
+            message.metadata,
+            {
+                "voice": {
+                    "file": "/usr/local/wamedia/shared/463e/b7ec/ff4e4d9bb1101879cbd41",
+                    "id": "463eb7ec-ff4e-4d9b-b110-1879cbd411b2",
+                    "mime_type": "audio/ogg; codecs=opus",
+                    "sha256": "fa9e1807d936b7cebe63654ea3a7912b1fa9479220258d823590521",
+                },
+                "timestamp": "1521827831",
+                "type": "voice",
+            },
+        )
+
+    def test_handle_contacts(self):
+        """
+        Returns a UserMessage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "contacts": [
+                    {
+                        "addresses": [
+                            {
+                                "city": "Menlo Park",
+                                "country": "United States",
+                                "country_code": "us",
+                                "state": "CA",
+                                "street": "1 Hacker Way",
+                                "type": "WORK",
+                                "zip": "94025",
+                            }
+                        ],
+                        "birthday": "2012-08-18",
+                        "contact_image": "/9j/4AAQSkZJRgABAQEAZABkAAD/2wBDAAgGBgcGBQgH",
+                        "emails": [{"email": "kfish@fb.com", "type": "WORK"}],
+                        "ims": [{"service": "AIM", "user_id": "kfish"}],
+                        "name": {
+                            "first_name": "Kerry",
+                            "formatted_name": "Kerry Fisher",
+                            "last_name": "Fisher",
+                        },
+                        "org": {"company": "Facebook"},
+                        "phones": [
+                            {"phone": "+1 (940) 555-1234", "type": "CELL"},
+                            {
+                                "phone": "+1 (650) 555-1234",
+                                "type": "WORK",
+                                "wa_id": "16505551234",
+                            },
+                        ],
+                        "urls": [{"url": "https://www.facebook.com", "type": "WORK"}],
+                    }
+                ],
+                "from": "16505551234",
+                "id": "ABGGFlA4dSRvAgo6C4Z53hMh1ugR",
+                "timestamp": "1537248012",
+                "type": "contacts",
+            }
+        )
+        self.assertEqual(message.text, None)
+        self.assertEqual(message.sender_id, "16505551234")
+        self.assertEqual(message.message_id, "ABGGFlA4dSRvAgo6C4Z53hMh1ugR")
+        self.assertEqual(
+            message.metadata,
+            {
+                "contacts": [
+                    {
+                        "addresses": [
+                            {
+                                "city": "Menlo Park",
+                                "country": "United States",
+                                "country_code": "us",
+                                "state": "CA",
+                                "street": "1 Hacker Way",
+                                "type": "WORK",
+                                "zip": "94025",
+                            }
+                        ],
+                        "birthday": "2012-08-18",
+                        "contact_image": "/9j/4AAQSkZJRgABAQEAZABkAAD/2wBDAAgGBgcGBQgH",
+                        "emails": [{"email": "kfish@fb.com", "type": "WORK"}],
+                        "ims": [{"service": "AIM", "user_id": "kfish"}],
+                        "name": {
+                            "first_name": "Kerry",
+                            "formatted_name": "Kerry Fisher",
+                            "last_name": "Fisher",
+                        },
+                        "org": {"company": "Facebook"},
+                        "phones": [
+                            {"phone": "+1 (940) 555-1234", "type": "CELL"},
+                            {
+                                "phone": "+1 (650) 555-1234",
+                                "type": "WORK",
+                                "wa_id": "16505551234",
+                            },
+                        ],
+                        "urls": [{"url": "https://www.facebook.com", "type": "WORK"}],
+                    }
+                ],
+                "timestamp": "1537248012",
+                "type": "contacts",
+            },
+        )
+
+    def test_handle_location(self):
+        """
+        Returns a UserMessage with valid parameters
+        """
+        message = self.input_channel.extract_message(
+            {
+                "from": "16315551234",
+                "id": "ABGGFlA5FpafAgo6tHcNmNjXmuSf",
+                "location": {
+                    "address": "Main Street Beach, Santa Cruz, CA",
+                    "latitude": 38.9806263495,
+                    "longitude": -131.9428612257,
+                    "name": "Main Street Beach",
+                    "url": "https://foursquare.com/v/4d7031d35b5df7744",
+                },
+                "timestamp": "1521497875",
+                "type": "location",
+            }
+        )
+        self.assertEqual(message.text, None)
+        self.assertEqual(message.sender_id, "16315551234")
+        self.assertEqual(message.message_id, "ABGGFlA5FpafAgo6tHcNmNjXmuSf")
+        self.assertEqual(
+            message.metadata,
+            {
+                "location": {
+                    "address": "Main Street Beach, Santa Cruz, CA",
+                    "latitude": 38.9806263495,
+                    "longitude": -131.9428612257,
+                    "name": "Main Street Beach",
+                    "url": "https://foursquare.com/v/4d7031d35b5df7744",
+                },
+                "timestamp": "1521497875",
+                "type": "location",
+            },
+        )
---
+++ turn_rasa_connector/turn.py
@@ -0,0 +1,129 @@
+import base64
+import hmac
+import json
+import logging
+from asyncio import wait
+from typing import Any, Awaitable, Callable, Dict, Optional, Text
+
+from rasa.core.channels import InputChannel, UserMessage
+from sanic import Blueprint, response
+from sanic.request import Request
+from sanic.response import HTTPResponse
+
+logger = logging.getLogger(__name__)
+
+
+class TurnInput(InputChannel):
+    """
+    Turn input channel
+    """
+
+    @classmethod
+    def name(cls) -> Text:
+        return "turn"
+
+    @classmethod
+    def from_credentials(cls, credentials: Dict[Text, Any]) -> InputChannel:
+        if not credentials:
+            cls.raise_missing_credentials_exception()
+
+        return cls(credentials.get("hmac_secret"))
+
+    def __init__(self, hmac_secret: Optional[Text]) -> None:
+        self.hmac_secret = hmac_secret
+
+    def blueprint(
+        self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
+    ) -> Blueprint:
+        turn_webhook = Blueprint("turn_webhook", __name__)
+
+        @turn_webhook.route("/", methods=["GET"])
+        async def health(request: Request) -> HTTPResponse:
+            return response.json({"status": "ok"})
+
+        @turn_webhook.route("/webhook", methods=["POST"])
+        async def webhook(request: Request) -> HTTPResponse:
+            if self.hmac_secret:
+                signature = request.headers.get("X-Turn-Hook-Signature") or ""
+                valid_signature = self.validate_signature(
+                    self.hmac_secret, request.body, signature
+                )
+                if not valid_signature:
+                    return response.json(
+                        {"success": False, "error": "invalid_signature"}, status=401
+                    )
+            else:
+                logging.warning("hmac_secret config not set, not validating signature")
+
+            try:
+                messages = request.json["messages"]
+                assert isinstance(messages, list)
+            except (TypeError, KeyError, AssertionError):
+                return response.json(
+                    {"success": False, "error": "invalid_body"}, status=400
+                )
+
+            user_messages = []
+            for message in messages:
+                try:
+                    user_messages.append(self.extract_message(message))
+                except (TypeError, KeyError, AttributeError):
+                    logger.warning(f"Invalid message: {json.dumps(message)}")
+                    return response.json(
+                        {"success": False, "error": "invalid_message"}, status=400
+                    )
+
+            await wait(list(map(on_new_message, user_messages)))
+            return response.json({"success": True})
+
+        return turn_webhook
+
+    @staticmethod
+    def validate_signature(secret: Text, payload: bytes, signature: Text) -> bool:
+        decoded_secret = secret.encode("utf8")
+        decoded_signature = base64.b64decode(signature)
+        digest = hmac.new(decoded_secret, payload, "sha256").digest()
+        return hmac.compare_digest(digest, decoded_signature)
+
+    def extract_message(self, message: dict) -> UserMessage:
+        message_type = message["type"]
+        handler = getattr(self, f"handle_{message_type}")
+        return handler(message)
+
+    def handle_common(self, text: Optional[str], message: dict) -> UserMessage:
+        return UserMessage(
+            text=text,
+            # TODO: Create output channel for responses
+            output_channel=None,
+            sender_id=message.pop("from"),
+            input_channel=self.name(),
+            message_id=message.pop("id"),
+            metadata=message,
+        )
+
+    def handle_text(self, message: dict) -> UserMessage:
+        return self.handle_common(message.pop("text")["body"], message)
+
+    def handle_media(self, media_type: str, message: dict) -> UserMessage:
+        return self.handle_common(message[media_type].pop("caption", None), message)
+
+    def handle_audio(self, message: dict) -> UserMessage:
+        return self.handle_media("audio", message)
+
+    def handle_document(self, message: dict) -> UserMessage:
+        return self.handle_media("document", message)
+
+    def handle_image(self, message: dict) -> UserMessage:
+        return self.handle_media("image", message)
+
+    def handle_video(self, message: dict) -> UserMessage:
+        return self.handle_media("video", message)
+
+    def handle_voice(self, message: dict) -> UserMessage:
+        return self.handle_media("voice", message)
+
+    def handle_contacts(self, message: dict) -> UserMessage:
+        return self.handle_common(None, message)
+
+    def handle_location(self, message: dict) -> UserMessage:
+        return self.handle_common(None, message)
